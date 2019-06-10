e0c1818f4a53fccf03441dbdda8baa8947c522f0
eastend-street <ss_1322irf@icloud.com>
2019-06-10 15:44:16 -0700

    implement Welcome to Java

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,7 +2,7 @@
 public class MyClass {
 	public static void main(String[] args) {
 		Hello.doSomething("This is a test");
-		
+
 		Hello hello2 = new Hello();
 		hello2.doSomethingelse("This is correct.");
 	}
