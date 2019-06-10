9ecd7c15222a2d5622427b1350babcc6ef84b0c6
eastend-street <ss_1322irf@icloud.com>
2019-06-10 15:46:36 -0700

    change Welcome to Java to PrintFiveTimes

--- src/PrintFiveTimes.java
+++ src/PrintFiveTimes.java
@@ -1,7 +1,7 @@
 
-public class WelcomeToJava {
+public class PrintFiveTimes {
 	public static void main(String[] args) {		
-		for (int i = 1; i <= 5; i++) {
+		for (int i = 0; i < 5; i++) {
 			System.out.println("Welcome to Java");
 		}
 	}
